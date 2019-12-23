95553066dce590d4131a6c68f4224b3a08586986
AlexVipond <alex.r.vipond@gmail.com>
2019-12-23 01:21:04 -0600

    specify gesture version

--- src/index.js
+++ src/index.js
@@ -1,61 +1,67 @@
-import clicks from './factories/clicks'
-import drag from './factories/drag'
-import dragdrop from './factories/dragdrop'
-import pan from './factories/pan'
-import pinch from './factories/pinch'
-import press from './factories/press'
-import rotate from './factories/rotate'
-import swipe from './factories/swipe'
-import taps from './factories/taps'
+import clicksFactory from './factories/clicks'
+import dragFactory from './factories/drag'
+import dragdropFactory from './factories/dragdrop'
+import panFactory from './factories/pan'
+import pinchFactory from './factories/pinch'
+import pressFactory from './factories/press'
+import rotateFactory from './factories/rotate'
+import swipeFactory from './factories/swipe'
+import tapsFactory from './factories/taps'
 
 function recognized (event, object) {
   return object.handle(event) === 'recognized'
 }
 
-export default {
-  clicks: {
-    factory: clicks,
-    events: ['mousedown', 'mousemove', 'mouseout', 'mouseup'],
-    recognized,
-  },
-  drag: {
-    factory: drag,
-    events: ['mousedown', 'mousemove', 'mouseout', 'mouseup'],
-    recognized,
-  },
-  dragdrop: {
-    factory: dragdrop,
-    events: ['mousedown', 'mousemove', 'mouseout', 'mouseup'],
-    recognized,
-  },
-  pan: {
-    factory: pan,
-    events: ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
-    recognized,
-  },
-  pinch: {
-    factory: pinch,
-    events: ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
-    recognized,
-  },
-  press: {
-    factory: press,
-    events: ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
-    recognized,
-  },
-  rotate: {
-    factory: rotate,
-    events: ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
-    recognized,
-  },
-  swipe: {
-    factory: swipe,
-    events: ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
-    recognized,
-  },
-  taps: {
-    factory: taps,
-    events: ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
-    recognized,
-  },
+export const clicks = {
+  factory: clicksFactory,
+  events: ['mousedown', 'mousemove', 'mouseout', 'mouseup'],
+  recognized,
+}
+
+export const drag = {
+  factory: dragFactory,
+  events: ['mousedown', 'mousemove', 'mouseout', 'mouseup'],
+  recognized,
+}
+
+export const dragdrop = {
+  factory: dragdropFactory,
+  events: ['mousedown', 'mousemove', 'mouseout', 'mouseup'],
+  recognized,
+}
+
+export const pan = {
+  factory: panFactory,
+  events: ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
+  recognized,
+}
+
+export const pinch = {
+  factory: pinchFactory,
+  events: ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
+  recognized,
+}
+
+export const press = {
+  factory: pressFactory,
+  events: ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
+  recognized,
+}
+
+export const rotate = {
+  factory: rotateFactory,
+  events: ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
+  recognized,
+}
+
+export const swipe = {
+  factory: swipeFactory,
+  events: ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
+  recognized,
+}
+
+export const taps = {
+  factory: tapsFactory,
+  events: ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
+  recognized,
 }
